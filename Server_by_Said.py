6fa38159fc44dcb95681ea06e943feab0889761f
SAİD AGHAMALİYEV <digitalwarior666@gmail.com>
2025-09-13 16:41:04 +0400

    Update Server_by_Said.py

--- Server_by_Said.py
+++ Server_by_Said.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 new_server = Flask(__name__)
 CORS(new_server)
 
-genai.configure(api_key="AIzaSyA4GOTW7dfyZr5x09kTpAKEiN3y56Wi13Y")
+genai.configure(api_key="AIzaSyBmPrCO2_J4K6a6XWVy56xhr72JLr6JLy4")
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 @new_server.route("/analyze", methods=["POST"])
@@ -13,7 +13,7 @@ def analyze():
     if request.method=="POST":
         
             msg = request.json["text"]
-            mode=request.json["mode"]
+            mode=request.json["mod"]
             if not msg:
                 return jsonify({"error": "No data provided"}), 400
             
@@ -27,25 +27,28 @@ def analyze():
                 prompt = (
                     f"Explain the following text clearly and factually in the style of Wikipedia. "
                     f"Keep the explanation under 50 words. Do not ask questions or provide extra commentary. "
+                    f"Don't use text format (ex. *[text]*)."
                     f"Just provide concise information.\n\nText: {msg}"
+                   
                 )
-            elif mode == "more":
+            elif mode == "academic":
                 prompt = (
-                    f"Explain the following text in detail as if writing a Wikipedia article section. "
-                    f"Provide historical or contextual background if relevant. "
+                    f"Explain the following text in detail using Google Scholar sources."
+                    f"Provide historical or contextual background if relevant."
+                    f"Don't use text format (ex. *[text]*)."
                     f"Keep it under 150 words. Include more technical details and examples if applicable.\n\nText: {msg}"
+                    
                 )
             else:
                 return jsonify({"error": "Invalid mode specified"}), 400
 
             response = model.generate_content(prompt)
             return jsonify({
-                "status": "success",
                 "answer": response.text,
-                "mode": mode
             }), 200
 
 
 
 if __name__ == "__main__":
     new_server.run(debug=True)
+
